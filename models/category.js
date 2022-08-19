bd671929452aa4677f2996c169297a6d926a74f4
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-08-19 16:33:29 -0300

    Se agrega asociación con News

--- models/category.js
+++ models/category.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Category.hasMany(models.News, { foreignKey: 'categoryId', as: 'news' });
     }
   }
   Category.init({
