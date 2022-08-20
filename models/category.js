ff236ceaf116961652955cd3be61d8b34cd4064f
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-08-20 17:39:20 -0300

    Se comentan la relacion de News a Category

--- models/category.js
+++ models/category.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Category.hasMany(models.News, { foreignKey: 'categoryId', as: 'news' });
+      //Category.hasMany(models.News, { foreignKey: 'categoryId', as: 'news' });
     }
   }
   Category.init({
