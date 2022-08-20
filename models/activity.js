08ae25830bb1aba4f6e0259251f8889a61a4a488
Miguel Fuenzalida <miguelfuenzalida.n@gmail.com>
2022-08-20 15:43:50 -0400

    migration de Activity arreglado

--- models/activity.js
+++ models/activity.js
@@ -4,17 +4,23 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class Activity extends Model {
-
-        static associate(models) {}
-    };
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
     Activity.init({
         name: DataTypes.STRING,
         content: DataTypes.STRING,
         image: DataTypes.STRING,
-        deletedAt: DataTypes.DATE,
+        softDelete: DataTypes.BOOLEAN
     }, {
         sequelize,
-        modelName: 'Activiy',
+        modelName: 'Activity',
         paranoid: true
     });
     return Activity;
