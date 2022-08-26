8b517550fd4097a3effcf8fd5ae4d7bbb8803650
Miguel Fuenzalida <miguelfuenzalida.n@gmail.com>
2022-08-26 19:25:49 -0400

    Endpoint Activities Added

--- routes/activity.js
+++ routes/activity.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const { activityGet, activityPost, activityUpdate, activityDelete } = require('../controllers/activityController');
+const { activityFormValidate } = require('../utils/validators/ActivityForm/createActivity');
 
 /* GET activities listing. */
 router.get('/', activityGet);
-router.post('/', activityPost);
+router.post('/', activityFormValidate, activityPost);
 router.put('/:id', activityUpdate);
 router.delete('/:id', activityDelete);
 
