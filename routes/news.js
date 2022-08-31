496074195275698676dd01391c556c4c4676359b
Miguel Fuenzalida <miguelfuenzalida.n@gmail.com>
2022-08-31 17:35:53 -0400

    Validate Admin in POST News, and validate empty fields of News

--- routes/news.js
+++ routes/news.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 const { getAll, getById, createNews, putNews, deleteNews } = require('../controllers/newsController');
 const { verifyAdmin } = require('../middleware/verifyAdmin');
+const { newsFormValidate } = require('../utils/validators/NewsForm/createNews');
 
 /* GET categories listing. */
 router.get('/', getAll);
 router.get('/:id', verifyAdmin, getById);
-router.post('/new/', createNews);
+router.post('/news/', verifyAdmin, newsFormValidate, createNews);
 router.put('/update/:id', putNews);
 router.delete('/delete/:id', deleteNews);
 
---
+++ utils/validators/NewsForm/createNews.js
@@ -0,0 +1,22 @@
+const { check } = require('express-validator');
+const { validateResult } = require('./validateNewsResult')
+
+const newsFormValidate = [
+    check('name', 'Please use the variable name')
+    .exists()
+    .notEmpty().withMessage('Empty field, Please enter activity name'),
+
+    check('content', 'Please use the variable content')
+    .exists()
+    .notEmpty().withMessage('Empty field, Please enter a content'),
+
+    check('news', 'Please use the variable news')
+    .exists()
+    .notEmpty().withMessage('Empty field, Please enter a news'),
+
+    (req, res, next) => {
+        validateResult(req, res, next)
+    }
+]
+
+module.exports = { newsFormValidate }
