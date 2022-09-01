e06e85695cc027860be17cb146f9521a0cb5d1ad
jimalaros <jimalaros25@gmail.com>
2022-09-01 16:51:45 -0500

    Endpoint to list Member's attributes

--- routes/member.routes.js
+++ routes/member.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', Members.listMembers);
 
-router.get('/', verifyAdmin, Members.listMembersAttributes);
+router.get('/attributes', verifyAdmin, Members.listMembersAttributes);
 
 router.post('/new', Members.createMember);
 
