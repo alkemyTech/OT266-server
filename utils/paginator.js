334940fa676bc4a32d9d76ebc2ebc700aef004c0
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-09-07 19:09:41 -0300

    Funciones de paginacion
    getPagination recibe el page y el size, y devuelve los valores limit y offset para ser usado en la consulta en sequalize.
    
    getPagingData formatea el JSON para mandar en la respuesta

---
+++ utils/paginator.js
@@ -0,0 +1,39 @@
+const getPagination = (page, size) => {
+    const limit = size ? +size : 3;
+    const offset = page > 0 ? (page-1) * limit : 0;
+  
+    return { limit, offset };
+};
+  
+const getPagingData = (data, page, limit, entity) => {
+    const { count: totalItems, rows: items } = data;
+    const currentPage = page ? +page : 1;
+    const totalPages = Math.ceil(totalItems / limit);
+
+    const url = "http://localhost:3000/";
+
+    const nextPage = 
+        page < totalPages 
+        ? url + `${entity}/?page=${parseInt(page)+1}`
+        : null;
+
+    const prevPage = 
+        page > 1 
+        ? url + `${entity}/?page=${(page)-1}`
+        : null;
+
+    return { 
+        totalItems, 
+        items, 
+        totalPages, 
+        currentPage,
+        nextPage,
+        prevPage 
+    };
+};
+  
+
+module.exports = {
+    getPagination,
+    getPagingData
+}
