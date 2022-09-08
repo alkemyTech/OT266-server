e0f3eaf577ffc39f99c384a13e2fbfe6e6fe0ef5
guidobursz <guidobursztyn@hotmail.com>
2022-09-08 14:57:46 -0300

    Creo ruta post /comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,3 +1,10 @@
 //Importo modelos
 const { Comment } = require('../db/models/index');
 
+//Handlers for routes
+const commentsPost = (req,res) => {
+    res.send('post de /comments')
+}
+
+
+module.exports = {commentsPost}
--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+//Import handlers
+const {
+    commentsPost
+} = require('../controllers/commentController')
 
 //Routes
-
+router.post('/', commentsPost)
 
 module.exports = router;
