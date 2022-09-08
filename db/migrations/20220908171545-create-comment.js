e65d8267cc387b1765521e682e9c338d9f4696ac
guidobursz <guidobursztyn@hotmail.com>
2022-09-08 14:18:59 -0300

    First config model/migration
    Add paranoid in model
    Add deleted at in migration

--- db/migrations/20220908171545-create-comment.js
+++ db/migrations/20220908171545-create-comment.js
@@ -17,6 +17,9 @@ module.exports = {
       news_id: {
         type: Sequelize.INTEGER
       },
+      deletedAt: {
+        type: Sequelize.DATE
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- db/models/comment.js
+++ db/models/comment.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Comment',
+    paranoid: true
   });
   return Comment;
 };
