eaafaabec29b90d639b7743e9565102ee8ab6626
guidobursz <guidobursztyn@hotmail.com>
2022-09-13 19:16:14 -0300

    Fix original paginator

--- utils/paginator.js
+++ utils/paginator.js
@@ -1,39 +1,65 @@
-const getPagination = (page, size) => {
+//Build complete url with the entire req object
+const getUrl = (reqbody) => {
+    return `${reqbody.protocol}//${reqbody.get('Host')}${reqbody.baseUrl}`
+}
+
+//Define query for db
+const getPagination = (page, size, ) => {
     const limit = size ? +size : 10;
+    
     const offset = page > 0 ? (page - 1) * limit : 0;
 
     return { limit, offset };
 };
 
-const getPagingData = (data, page, limit, entity) => {
+//GetPagingData receive: 
+    //data = model.findAndCountAll result
+    //page = const { page = 1, size } = req.query;
+    //limit = const { page = 1, size } = req.query;
+    //link =  const url : `${req.protocol}//${req.get('Host')}${req.baseUrl}`
+const getPagingData = (data, page, limit, link) => {
     const { count: totalItems, rows: items } = data;
-    const currentPage = page ? +page : 1;
+    
     const totalPages = Math.ceil(totalItems / limit);
+    
+    const url = link;
 
-    const url = "http://localhost:3000/";
+    //Send currentpage number to check if current page > totalPages
+    let currentPage = page;
+    if(currentPage == 0 ){
+        currentPage = 1;
+    }else if(currentPage > 0){
+        currentPage = page
+    }
 
+    //Prev page link 
+    let prevPage;
+    if(currentPage == 0 || currentPage == 1 ) {
+        prevPage = null
+    }else if(currentPage <= totalPages){
+        prevPage = url + `/?page=${(currentPage)-1}`
+    }
+
+    //Next page link
     const nextPage =
         currentPage < totalPages ?
-        url + `${entity}/?page=${parseInt(currentPage)+1}` :
+        url + `/?page=${parseInt(currentPage)+1}` :
         null;
 
-    const prevPage =
-        currentPage > 1 ?
-        url + `${entity}/?page=${(currentPage)-1}` :
-        null;
 
     return {
         totalItems,
         items,
         totalPages,
+        prevPage,
         currentPage,
         nextPage,
-        prevPage
     };
 };
 
 
 module.exports = {
+    getUrl,
     getPagination,
     getPagingData
 }
