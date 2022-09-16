7e664a041f20ac017a954bc9811c77630c0824ae
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-09-16 15:55:08 -0300

    Se agrega testing a las rutas GetAll y GetOne de testimonios

--- test/testimony.test.js
+++ test/testimony.test.js
@@ -16,3 +16,80 @@ const testimonyTest = {
     content : 'test'
 }
 
+describe("GET /testimony", ()=>{
+    it("should return status 200", (done) => {
+         chai.request(server)
+            .get('/testimony')
+            .set({
+                Authorization: token
+            })
+            .end((err,res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                done()
+            })
+    })
+
+    it("should verify that there are 10 testimonies in the page", (done) => {
+        chai.request(server)
+        .get('/testimony')
+        .set({
+            Authorization: token
+        })
+        .end((err,res)=>{
+            res.body.should.be.a('object');
+            res.body.response.items.length.should.be.eql(10);
+            done()
+        })
+    })
+
+    it("should return an authorization error", (done) => {
+        chai.request(server)
+           .get('/testimony')
+           .end((err,res)=>{
+               res.should.have.status(403);
+               done()
+           })
+   })
+})
+
+describe("GET ONE /testimony", ()=>{
+
+    it("should return status 200", (done) => {
+         let token = process.env.TEST_TOKEN;
+         let id = 2;
+         chai.request(server)
+            .get('/testimony/' + id)
+            .set({
+                Authorization: token
+            })
+            .end((err,res)=>{
+                res.body.should.be.a('object');
+                res.body.should.have.property('testimony');
+                res.should.have.status(200);
+                done()
+            })
+    })
+
+    it("should return an authorization error", (done) => {
+        chai.request(server)
+           .get('/testimony/' + '2')
+           .end((err,res)=>{
+               res.should.have.status(403);
+               done()
+           })
+    })
+
+    it("should return status 404", (done) => {
+        let token = process.env.TEST_TOKEN;
+        chai.request(server)
+           .get('/testimony/' + '0')
+           .set({
+               Authorization: token
+           })
+           .end((err,res)=>{
+               res.should.have.status(404);
+               done()
+           })
+    })
+})
