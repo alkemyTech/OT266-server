ad48cc467ce94f6d6e26362a8ad760e3ae183dd2
guidobursz <guidobursztyn@hotmail.com>
2022-09-18 01:58:43 -0300

    Update auth.test.js
    Done login tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -26,16 +26,26 @@ const token = process.env.TEST_TOKEN;
         }
         //Object with incorrect email for login test
         const incorrectEmail = {
+            //email: "test@test.com",
             email: 'emailnotindb@nodb.com',
             password: '123'
         }
-
+        //Object with no password for login test
+        const noPasswordLogin = {
+            email: "test@test.com",
+            //password: '123'
+        }
+        //Object with invalid for login test
+        const invalidPasswordLogin = {
+            email: "test@test.com",
+            password: 'notrealpassword'
+        }
 
 
 //Create tests
-describe('Testing /auth/ login||register routes', ()=> {
+describe('Testing /auth/login routes', ()=> {
 
-    //First test: GET to /auth/login
+    //First login test: GET to /auth/login
     it('GET /auth/login should return status 200', (done) => {
         chai.request(server)
             .get('/auth/login')
@@ -45,7 +55,14 @@ describe('Testing /auth/ login||register routes', ()=> {
             })
     })
 
-    //Second test: POST to /auth/login successfully
+    //Cases for trying to login
+        //0) Correct login - done
+        //1) No email sent - done
+        //2) Invalid email - done
+        //3) no password sent - done
+        //4) Invalid password for the user
+
+    //Second login test: POST to /auth/login successfully
     it('POST /auth/login successfully should return status 200 & object', (done) => {
         chai.request(server)
             .post('/auth/login')
@@ -57,34 +74,53 @@ describe('Testing /auth/ login||register routes', ()=> {
             })
     })
 
-        //Thrid test: POST to /auth/login with no email
-        it('POST to /auth/login with no email should return 400', (done) => {
+        //Thrid login test: POST to /auth/login with no email
+        it('POST to /auth/login with no email should return 401 status  & an object', (done) => {
             chai.request(server)
                 .post('/auth/login')
                 .send(noEmailLogin)
                 .end((err, res) => {
-                    res.should.have.status(400);
+                    res.should.have.status(401);
                     res.should.be.a('object');
                     done()
                 })
         })
-/* 
-        //Forth test: POST to /auth/login with no registered email
-        it('POST to /auth/login with no registered email', (done) => {
+
+        //Fourth login test: POST to /auth/login with no registered email
+        it('POST to /auth/login with no registered email should return 401 status & an object', (done) => {
             chai.request(server)
                 .post('/auth/login')
                 .send(incorrectEmail)
                 .end((err, res) => {
-                    res.should.have.status(400);
-                    res.should.be.a('array');
+                    res.should.have.status(401);
+                    res.should.be.a('object');
                     done()
                 })
         })
-*/
 
+        //Fifth login test: POST to /auth/login with no password sent
+        it('POST to /auth/login with no password should return 401 status & an object', (done) => {
+            chai.request(server)
+                .post('/auth/login')
+                .send(noPasswordLogin)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    res.should.be.a('object');
+                    done()
+                })
+        })
 
-
-
-
-
+        //Sixth login test: POST to /auth/login with incorrect password
+        it('POST to /auth/login with incorrect password should return 401 status & an object', (done) => {
+            chai.request(server)
+                .post('/auth/login')
+                .send(invalidPasswordLogin)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    res.should.be.a('object');
+                    done()
+                })
+        })
 })
+
+
