f76570ce82cf9248ad5e95370b5927454e16d99b
Miguel Fuenzalida <miguelfuenzalida.n@gmail.com>
2022-09-20 18:03:05 -0400

    Activities tests

--- test/members.test.js
+++ test/members.test.js
@@ -20,69 +20,69 @@ describe("testing the members' endpoints", () => {
     }
 
     it('should verify that there 10 members in the page 1', (done) => {
-        let page=1;
+        let page = 1;
         chai.request(app)
-        .get('/members?' + page)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');      
-            done();
-        });
+            .get('/members?' + page)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                done();
+            });
     });
 
 
     it('should show some attributes of the members', (done) => {
         let token = process.env.TEST_TOKEN;
         chai.request(app)
-        .get('/members/attributes')
-        .set({
-            Authorization: token
-        })
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-            done();
-        });
+            .get('/members/attributes')
+            .set({
+                Authorization: token
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            });
     });
 
 
     it('should POST a valid member', (done) => {
         chai.request(app)
-        .post('/members')
-        .send(member)
-        .end((err, res) => {
-            res.should.have.status(201);
-            res.body.should.have.property('msg').eql('Member created successfully');
-            done();
-        });
+            .post('/members')
+            .send(member)
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.have.property('msg').eql('Member created successfully');
+                done();
+            });
     });
 
     it('it should UPDATE a member given the id', (done) => {
         chai.request(app)
-        .put('/members/update/' + 10)
-        .send({
-            nameMember: "test member",
-            facebookUrl: "https:/facebook.com/test_member",
-            instagramUrl: "https:/instagram.com/test_member",
-            linkedinUrl: "https:/linkedin.com/in/test_member",
-            image: "test_member.jpg",
-            description: "test"
-        })
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.member.should.have.property('nameMember').eql("test member");
-            done();
-        });
+            .put('/members/update/' + 9)
+            .send({
+                nameMember: "test member",
+                facebookUrl: "https:/facebook.com/test_member",
+                instagramUrl: "https:/instagram.com/test_member",
+                linkedinUrl: "https:/linkedin.com/in/test_member",
+                image: "test_member.jpg",
+                description: "test"
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.member.should.have.property('nameMember').eql("test member");
+                done();
+            });
     });
 
     it('it should DELETE a member given the id', (done) => {
         chai.request(app)
-        .delete('/members/delete/' + 8)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            done();
-        });
+            .delete('/members/delete/' + 59)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                done();
+            });
     });
 })
