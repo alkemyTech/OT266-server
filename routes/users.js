8b1f86dd6cca5d5ea269faebbe1d81454878fc85
jimalaros <jimalaros25@gmail.com>
2022-09-21 09:55:37 -0500

    fix swagger

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 //Import middleware
 const { verifyAdmin } = require('../middleware/verifyAdmin');
@@ -15,89 +15,10 @@ const {
 
 /* GET users listing. */
 
-/**
- * @swagger
- * /users:
- *  get:
- *      summary: To see all the users in the database
- *      tags: [Users]
- *      responses:
- *          200:
- *              description: Ok
- *          400:
- *              description: Bad Request
- */
 router.get('/', verifyAdmin, getAllUsersGET);
-
-/**
- * @swagger
- * /users/{id}:
- *  patch:
- *      summary: To update one user from the database
- *      tags: [Users]
- *      parameters:
- *          - name: id
- *            in: path
- *            required: true
- *            description: User's id
- *            schema: 
- *              type: number
- *              example: 1
- *      requestBody:
- *          required: true
- *          content:
- *              application/json:
- *                  schema:
- *                    type: object
- *                    required:
- *                      -firstName
- *                      -lastName
- *                      -email
- *                      -password
- *                    properties:
- *                      firstName:
- *                          type: string
- *                          example: "Jimmy"
- *                      lastName:
- *                          type: string
- *                          example: "Arango"
- *                      email:
- *                          type: string
- *                          example: "j@gmail.com"
- *                      password:
- *                          type: string
- *                          example: "123456" 
- *      responses:
- *          200:
- *              Ok
- *          400:
- *              description: Bad Request
- */
 router.patch('/:id', activeIdFilter, verifyUser, updateUserPATCH);
 //router.patch('/:id', activeIdFilter, (req,res)=>{res.send('testing')});
 
-//Ruta delete/:id
-
-/**
- * @swagger
- * /users/delete/{id}:
- *  delete:
- *      summary: To delete one category from the database
- *      tags: [Users]
- *      parameters:
- *          - name: id
- *            in: path
- *            required: true
- *            description: User's id
- *            schema: 
- *              type: number
- *              example: 1
- *      responses:
- *          200:
- *              Ok
- *          400:
- *              description: Bad Request
- */
 router.delete('/delete/:id', verifyUser, deleteUserById);
 
 module.exports = router;
